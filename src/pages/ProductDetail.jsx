1f0bbf214b9f0ea9be3930489d0e44194e93e4fd
trizentautomotive1-design <trizentautomotive1@gmail.com>
2026-01-22 21:18:10 -0800

    new

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -329,9 +329,9 @@ function ProductDetail() {
                   </div>
                   <div className="flex items-center gap-1.5">
                     <svg className="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <span>Secure Checkout</span>
+                    <span>Deliver in 7 days</span>
                   </div>
                 </div>
               </div>
