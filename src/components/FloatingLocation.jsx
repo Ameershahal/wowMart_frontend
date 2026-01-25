c8316b662d4ac3714b12a2b1bb1a9b3af2e9655d
trizentautomotive1-design <trizentautomotive1@gmail.com>
2026-01-25 11:54:16 -0800

    n

--- src/components/FloatingLocation.jsx
+++ src/components/FloatingLocation.jsx
@@ -25,9 +25,6 @@ function FloatingLocation() {
       }}
     >
       <div className="relative">
-        {/* Pulse animation ring */}
-        <div className="absolute inset-0 bg-red-500 rounded-full animate-ping opacity-75"></div>
-        
         {/* Main button */}
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
